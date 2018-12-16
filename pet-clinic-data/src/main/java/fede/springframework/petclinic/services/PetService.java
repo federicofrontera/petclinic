5803871b0cefdeb5a9efbeafaa8a2a6125c03c64
federicofrontera <federicofrontera@gmail.com>
2018-12-16 19:15:30 -0300

    Refactored Service Interfaces to Common Base Interface. Closes #11.

--- pet-clinic-data/src/main/java/fede/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/services/PetService.java
@@ -4,12 +4,6 @@ import fede.springframework.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long>{
 
 }

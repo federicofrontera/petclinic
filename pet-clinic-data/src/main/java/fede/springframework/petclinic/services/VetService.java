5e22abce27941afadc2249b547a902af57ba7804
federicofrontera <federicofrontera@gmail.com>
2018-12-16 18:57:40 -0300

    Added Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package fede.springframework.petclinic.services;
+
+import fede.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

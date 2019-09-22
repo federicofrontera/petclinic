5a793c7995343c1a02696017c4f23e8e1fe3b185
federicofrontera <federicofrontera@gmail.com>
2019-09-21 21:59:34 -0300

    added Specialty map service. Closes #32.

---
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package fede.springframework.petclinic.services;
+
+import fede.springframework.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}

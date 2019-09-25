781571654f752205a1ae246130fe687abae2d58d
federicofrontera <federicofrontera@gmail.com>
2019-09-25 17:23:14 -0300

    added Visit map service. Closes #47.

---
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package fede.springframework.petclinic.services;
+
+import fede.springframework.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package fede.springframework.petclinic.services.map;
+
+import fede.springframework.petclinic.model.Visit;
+import fede.springframework.petclinic.repositories.VisitRepository;
+import fede.springframework.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if((visit.getPet() == null) || visit.getPet().getId() == null ||
+                visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}

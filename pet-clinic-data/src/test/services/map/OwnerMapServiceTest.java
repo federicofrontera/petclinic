8fe4944462c3884a4b24d5ec39c1cf903722edac
federicofrontera <federicofrontera@gmail.com>
2019-12-16 11:56:02 -0300

    reorganized test folder structure

---
+++ pet-clinic-data/src/test/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,37 @@
+package services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}

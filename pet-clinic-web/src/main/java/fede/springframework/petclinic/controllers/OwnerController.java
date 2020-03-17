c38f1ada37d2081a2e5af6621ae2912f571bc89e
federicofrontera <federicofrontera@gmail.com>
2020-03-17 18:29:18 -0300

    added controller method for showing owners by id. #54

--- pet-clinic-web/src/main/java/fede/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/fede/springframework/petclinic/controllers/OwnerController.java
@@ -1,10 +1,14 @@
 package fede.springframework.petclinic.controllers;
 
+import fede.springframework.petclinic.model.Owner;
 import fede.springframework.petclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -26,5 +30,16 @@ public class OwnerController {
     @RequestMapping({"/find"})
     public String findOwners(){
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = this.ownerService.findById(ownerId);
+/*        for (Pet pet : owner.getPets()) {
+            pet.setVisitsInternal(visits.findByPetId(pet.getId()));
+        }*/
+        mav.addObject(owner);
+        return mav;
     }
 }

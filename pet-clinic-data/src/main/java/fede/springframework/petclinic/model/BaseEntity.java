572cee5a354613021f129013f4878c8cafa0e77f
federicofrontera <federicofrontera@gmail.com>
2020-03-17 18:31:16 -0300

    added id parameter to owner/person constructors. needed for building objects with valid id values. #54

--- pet-clinic-data/src/main/java/fede/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/model/BaseEntity.java
@@ -1,5 +1,10 @@
 package fede.springframework.petclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -7,16 +12,12 @@ import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
 }

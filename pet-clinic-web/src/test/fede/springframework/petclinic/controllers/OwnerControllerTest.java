06ee93e562dab3ecf17060c6d6b376a79486cff6
federicofrontera <federicofrontera@gmail.com>
2020-03-17 18:27:58 -0300

    test for displayOwners mapping. #54

--- pet-clinic-web/src/test/fede/springframework/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/fede/springframework/petclinic/controllers/OwnerControllerTest.java
@@ -14,8 +14,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -71,7 +73,13 @@ class OwnerControllerTest {
     }
 
     @Test
-    void fakeTest(){
-        assertTrue(true);
+    void displayOwners() throws Exception {
+        Owner owner = Owner.builder().id(1l).build();
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
     }
 }

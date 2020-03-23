61db46f26f4b2108554ee33515ccb3746f83db59
federicofrontera <federicofrontera@gmail.com>
2020-03-23 16:43:51 -0300

    implemented builder pattern for Pet and PetType.#57.

--- pet-clinic-data/src/main/java/fede/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/fede/springframework/petclinic/model/Pet.java
@@ -12,17 +12,18 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+
+
 
     @Column(name = "name")
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "type_id")
-    private PetType type;
+    private PetType petType;
 
     @ManyToOne
     @JoinColumn(name = "owner_id")
@@ -33,4 +34,17 @@ public class Pet extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
 }

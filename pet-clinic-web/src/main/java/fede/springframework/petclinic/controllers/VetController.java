4359ad10014301100df11c932383df4869e8bf49
federicofrontera <federicofrontera@gmail.com>
2020-03-29 13:23:31 -0300

    added Vet JSON endpoint. Closes #68.

--- pet-clinic-web/src/main/java/fede/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/fede/springframework/petclinic/controllers/VetController.java
@@ -1,9 +1,15 @@
 package fede.springframework.petclinic.controllers;
 
+import fede.springframework.petclinic.model.Vet;
 import fede.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +24,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
+
     }
 }
